13a68e77269cd61dadbf8eedd2005b46a2e08244
angie-xu <axu5104@gmail.com>
2022-03-05 03:10:53 -0500

    up

--- week4/script.js
+++ week4/script.js
@@ -1,59 +1,22 @@
-function getFortune() {
-  let numElement = O('pickNumber') // get the element
-  let num = parseInt(numElement.value) //get its value and
-  //convert it to a number
-  let nameElement = O('userName')
-  let name = nameElement.value
-  let fortuneString = 'You will'
-  let fortune = O('fortune')
-  //in an if/else statement, everything should evaluate to true or false
-  //
-  if (name == 'Brent') {
-    fortuneString += ' be a professor'
-  }
+let fortunes = [
+  // add more array items here if you want, make sure you put a comma between each one. You can use HTML here! Just remember that if it contains quotes, they need to be escaped, like this: \"
+  "You will win a <em>million</em> dollars! Yay!",
+  "You will step on a piece of gum.",
+  "You will be attacked by a fluffy bunny."
+];
 
-  if (num >= 2) {
-    fortuneString += ', find true love'
-  } else if (num > 0) {i
-    fortuneString += ', win great riches'
-  } else {
-    fortuneString += ', become a pauper'
-  }
+let emojis = [
+  "⭐",
+  "🌙",
+  "☀️"
+];
 
-  switch (num) {
-    case 0:
-      fortuneString += ', and wander the earth forever'
-      break; //this ends execution for this case - if other cases are true and you don't include a break statement, they can get executed too
+function tellFortune() {
 
-    case 1:
-      fortuneString += ', and develop superpowers'
-      break;     case 2:
-      fortuneString += ', and end up marooned at sea'
-      break;
-    case 3:
-      fortuneString += ', and catch malaria'
-      break;
-    case 4:
-      fortuneString += ', and become Poseidon, Emperor of the Seas'
-      break;
-    case 5:
-      fortuneString += ', and die a horrible death'
-      // adding a style in a switch case - dying a horrible death is no fun!
-      fortune.style.backgroundColor = 'red'
-      break;
+  // generate a random number between 0 and the number of the array items
+  let randomNumber = Math.floor(Math.random() * fortunes.length);
+  let randomNum = Math.floor(Math.random() * emojis.length);
 
-  }
-  if (name == "Brent" && num ==3 ) {
-    fortuneString += ' and write a lot of code this week'
-  }
-
-  if (num == 2 || name == 'Dave') {
-    fortuneString += ' and be named Dave' //everyone named Dave picks 2, that's just a fact.
-  }
-  // set the inner text of the fortune element to our new string
-  // you could also try HTML elements instead of just adding the text!
-  fortune.innerText = fortuneString
-  // and don't forget to clear out the inputs for the next fortune
-  numElement.value = 0;
-  nameElement.value = ''
+  // take the fortune at the random spot in the array and insert it into the "fortune" div
+ document.getElementById("fortune").innerHTML = "<br>"+ emojis[randomNum] +"<br>"+"<br>"+ fortunes[randomNumber];
 }
