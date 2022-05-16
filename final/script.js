b511583712c036cfef0de209f5bacfee3fe27c1c
angie-xu <axu5104@gmail.com>
2022-05-16 16:11:04 -0400

    up

--- final/script.js
+++ final/script.js
@@ -18,63 +18,63 @@ var zodiacsign = inputVal;
 
 if (zodiacsign === 'aries' || zodiacsign ==='Aries') {
 console.log('It\'s Aries!');
-document.getElementById("div1").innerHTML= "<img src='img/Aries.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Aries.png'>";
 
 }
 
 else if (zodiacsign === 'taurus' || zodiacsign ==='Taurus') {
 console.log('It\'s Taurus!');
-document.getElementById("div1").innerHTML= "<img src='img/Taurus.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Taurus.png'>";
 }
 
 else if (zodiacsign === 'gemini' || zodiacsign ==='Gemini') {
 console.log('It\'s Gemini!');
-document.getElementById("div1").innerHTML= "<img src='img/Gemini.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Gemini.png'>";
 }
 
 else if (zodiacsign === 'cancer' || zodiacsign ==='Cancer') {
 console.log('It\'s Cancer!');
-document.getElementById("div1").innerHTML= "<img src='img/Cancer.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Cancer.png'>";
 }
 
 else if (zodiacsign === 'leo' || zodiacsign ==='Leo') {
 console.log('It\'s Leo!');
-document.getElementById("div1").innerHTML= "<img src='img/Leo.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Leo.png'>";
 }
 
 else if (zodiacsign === 'virgo' || zodiacsign ==='Virgo') {
 console.log('It\'s Virgo!');
-document.getElementById("div1").innerHTML= "<img src='img/Virgo.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Virgo.png'>";
 }
 
 else if (zodiacsign === 'libra' || zodiacsign ==='Libra') {
 console.log('It\'s Libra!');
-document.getElementById("div1").innerHTML= "<img src='img/Libra.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Libra.png'>";
 }
 
 else if (zodiacsign === 'scorpio' || zodiacsign ==='Scorpio') {
 console.log('It\'s Scorpio!');
-document.getElementById("div1").innerHTML= "<img src='img/Scorpio.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Scorpio.png'>";
 }
 
 else if (zodiacsign === 'sagittarius' || zodiacsign ==='Sagittarius') {
 console.log('It\'s Sagittarius!');
-document.getElementById("div1").innerHTML= "<img src='img/Sagittarius.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Sagittarius.png'>";
 }
 
 else if (zodiacsign === 'capricorn' || zodiacsign ==='Capricorn') {
 console.log('It\'s Capricorn!');
-document.getElementById("div1").innerHTML= "<img src='img/Capricorn.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Capricorn.png'>";
 }
     
 else if (zodiacsign === 'aquarius' || zodiacsign ==='Aquarius') {
 console.log('It\'s Aquarius!');
-document.getElementById("div1").innerHTML= "<img src='img/Aquarius.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Aquarius.png'>";
 }
     
 else if (zodiacsign === 'pisces' || zodiacsign ==='Pisces') {
 console.log('It\'s Pisces!');
-document.getElementById("div1").innerHTML= "<img src='img/Pisces.png'>";
+document.getElementById("div1").innerHTML= "<img src='img/Z-Pisces.png'>";
 }
 
 else{
